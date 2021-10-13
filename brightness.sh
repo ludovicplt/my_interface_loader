76799b9de5f030f6e377a056240962be02941a99
ludovicplt <ludovic.peltier@epitech.eu>
2021-10-14 01:18:28 +0400

    push %analytics

--- brightness.sh
+++ brightness.sh
@@ -3,22 +3,26 @@
 $DIALOG=${DIALOG=dialog}
 fichtmp=`tmpfile 2>/dev/null` || fichtmp=/tmp/test$$
 
+maxbr=$(cat /sys/class/backlight/intel_backlight/max_brightness)
+
 $DIALOG --clear --title "Brightness"	\
 	--menu "Hello $USER choose Brightness" 20 51 10 \
-	"2400" "low" \
-	"4800" "   " \
-	"7200" "   " \
-	"9600" "   " \
-	"12000" "average" \
-	"14400" "   " \
-	"16800" "   " \
-	"19800" "   " \
-	"21600" "   " \
-	"24000" "high" 2> $fichtmp
+	"10" "low" \
+	"20" "   " \
+	"30" "   " \
+	"40" "   " \
+	"50" "average" \
+	"60" "   " \
+	"70" "   " \
+	"80" "   " \
+	"90" "   " \
+	"100" "high" 2> $fichtmp
 valret=$?
 
+vartmp=$(($maxbr / 100 * $(cat $fichtmp)))
+
 case $valret in
-	0) clear && sudo cat $fichtmp > /sys/class/backlight/intel_backlight/brightness ;;
+	0) clear && sudo echo $vartmp > /sys/class/backlight/intel_backlight/brightness ;;
 	1|255) clear && echo "Aborted";;
 esac
 
